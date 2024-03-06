9bb6ac3b0f15e6970d2380e63e27845c9ef65deb
Jefferson Macena <jefferson_jazzs@hotmail.com>
2024-03-06 15:27:59 -0300

    Inicio

---
+++ src/test/java/com/itservice/project/ItServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itservice.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
